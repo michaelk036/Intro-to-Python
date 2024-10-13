4e9042b71d82e4a0d8c28074f24dd02234ec7c84
Michael <134699174+michaelk036@users.noreply.github.com>
2024-10-13 09:59:52 -0400

    Create 1_helloworld.py

---
+++ 1_helloworld.py
@@ -0,0 +1,16 @@
+#Michael Kovalev
+#8/30/23
+#This is (definetely) my first program
+
+print("Hello World!")
+print('My name is Michael')
+print("""
+   <0>   <0>
+       /
+      <3   
+    """)
+'''
+This is 
+a multi
+line comment
+'''
