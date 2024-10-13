ee58171dcada36d0c8e0e287fd616a74dc586097
Michael <134699174+michaelk036@users.noreply.github.com>
2024-10-13 10:00:21 -0400

    Create 2_Initials.py

---
+++ 2_Initials.py
@@ -0,0 +1,13 @@
+#Michael Kovalev
+#09.01.2024
+
+
+print("MM          MM KK     KK")
+print("MMMM      MMMM KK    KK")
+print("MMMMM    MMMMM KK  KK")
+print("MMMMMMM MMMMMM KKKK")
+print("MM  MMMMMMM MM KKKK")
+print("MM    MMM   MM KK  KK")
+print("MM     M    MM KK   KK")
+print("MM          MM KK    KK")
+print("MM          MM KK     KK")
